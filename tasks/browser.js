14f91a46ef4dd710056e3ea3d905c6d056be2164
huangqinlyh@163.com <huangqinlyh@163.com>
2017-04-29 19:56:53 +0800

    es6
    完成构建流程

--- tasks/browser.js
+++ tasks/browser.js
@@ -3,7 +3,7 @@ import gulpif from 'gulp-if';
 import gutil from 'gulp-util';
 import args from './util/args';
 
-gulp.task('browser',(cb)=>{
+gulp.task('browser',(cb) => {
   if(!args.watch) return cb();
   gulp.watch('app/**/*.js',['scripts']);
   gulp.watch('app/**/*.ejs',['pages']);
